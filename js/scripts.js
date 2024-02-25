fcd8f83f6e4cebe8f29871f92573f344f2e5bc1f
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2024-02-25 20:45:58 +0100

    Added error handling for background video not playing

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,18 @@ document.querySelector('video').addEventListener('canplaythrough', function() {
 document.querySelector('video').addEventListener('ended', function() {
     this.play();
 });
+
+/* Error handling the background video */
+
+const video = document.querySelector('video');
+
+// Listen for the error event on the video element
+video.addEventListener('error', function() {
+    console.error('Error loading or playing video');
+    
+    // Reset the video element by setting its currentTime to 0
+    video.currentTime = 0;
+    
+    // Play the video again
+    video.play();
+});
